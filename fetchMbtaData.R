0fdcf5d15c5c8fad6261139750024caf67b5cf62
Jeff Cunningham <jefrey@JeffsMBP2.local>
2016-04-26 12:07:15 -0400

    Looping over weeks, but not stations yet.

--- fetchMbtaData.R
+++ fetchMbtaData.R
@@ -6,11 +6,33 @@ library(jsonlite)
 library(dplyr)
 library(tidyr)
 library(readr)
+library(lubridate)
+
+TKeyJeff <- ""
+TKeyPerfDoc <- "?api_key=wX9NwuHnZU2ToO7GmGR9uw"
+TTravelURL <- "http://realtime.mbta.com/developer/api/v2.1/traveltimes"
+TFormat <- "&format=json"
+
+
+startTime <- as.POSIXct("2016-01-25 04:00:00")
+
+#foo <- fromJSON("http://realtime.mbta.com/developer/api/v2.1/traveltimes?api_key=wX9NwuHnZU2ToO7GmGR9uw&format=json&from_stop=70063&to_stop=70069&from_datetime=1444486210&to_datetime=1445091009")
 
 # We're going to pull 7 days worth of data, one pair of stations at a time.
 # So, first we need all the stations paired up along the routes.
 
+fromStop <- paste("&from_stop=", 70063, sep="")
+toStop <- paste("&to_stop=", 70069, sep="")
 
+# How many seven day periods from start to now?
+numWeeks <- as.integer(unclass(now() - startTime)/7)
+
+for(i in 0:numWeeks) {
+    fromTime <- paste("&from_datetime=", as.numeric(startTime + days(i * 7)), sep="")
+    toTime <- paste("&to_datetime=", as.numeric(startTime + days(i * 7) + days(7) - minutes(1)), sep="")
+    TRequest <- paste(TTravelURL, TKeyPerfDoc, TFormat, fromStop, toStop, fromTime, toTime, sep="")
+    foo <- fromJSON(TRequest)[[1]]
+}
 
 # We'll also want dwell times to identify trains just passing through.
 
