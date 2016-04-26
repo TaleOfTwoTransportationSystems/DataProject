c4c5d545c05f901b83d3d49dc1bc2d2c539d9334
Jeff Cunningham <jefrey@JeffsMBP2.local>
2016-04-26 12:33:19 -0400

    Pulling down the red line routes.

--- fetchMbtaData.R
+++ fetchMbtaData.R
@@ -1,6 +1,7 @@
 # Just a starting point for our CSCI E-107 group project
 # Step 1 -- pull down json travel time data from MBTA
 # https://groups.google.com/forum/#!topic/massdotdevelopers/z9scDvMCXDo
+#foo <- fromJSON("http://realtime.mbta.com/developer/api/v2.1/traveltimes?api_key=wX9NwuHnZU2ToO7GmGR9uw&format=json&from_stop=70063&to_stop=70069&from_datetime=1444486210&to_datetime=1445091009")
 
 library(jsonlite)
 library(dplyr)
@@ -9,17 +10,17 @@ library(readr)
 library(lubridate)
 
 TKeyJeff <- ""
-TKeyPerfDoc <- "?api_key=wX9NwuHnZU2ToO7GmGR9uw"
+TKeyAPIDoc <- "?api_key=wX9NwuHnZU2ToO7GmGR9uw"
+#http://realtime.mbta.com/developer/api/v2/stopsbyroute?api_key=wX9NwuHnZU2ToO7GmGR9uw&route=Red&format=json
+TRouteURL <- "http://realtime.mbta.com/developer/api/v2/stopsbyroute"
 TTravelURL <- "http://realtime.mbta.com/developer/api/v2.1/traveltimes"
 TFormat <- "&format=json"
-
-
+TRoutes <- c("Red", "Green", "Orange", "Blue")
 startTime <- as.POSIXct("2016-01-25 04:00:00")
-
-#foo <- fromJSON("http://realtime.mbta.com/developer/api/v2.1/traveltimes?api_key=wX9NwuHnZU2ToO7GmGR9uw&format=json&from_stop=70063&to_stop=70069&from_datetime=1444486210&to_datetime=1445091009")
 
 # We're going to pull 7 days worth of data, one pair of stations at a time.
 # So, first we need all the stations paired up along the routes.
+RedLineRoute <- fromJSON(paste(TRouteURL, TKeyAPIDoc, TFormat, "&route=Red", sep=""))[[1]]
 
 fromStop <- paste("&from_stop=", 70063, sep="")
 toStop <- paste("&to_stop=", 70069, sep="")
@@ -30,7 +31,7 @@ numWeeks <- as.integer(unclass(now() - startTime)/7)
 for(i in 0:numWeeks) {
     fromTime <- paste("&from_datetime=", as.numeric(startTime + days(i * 7)), sep="")
     toTime <- paste("&to_datetime=", as.numeric(startTime + days(i * 7) + days(7) - minutes(1)), sep="")
-    TRequest <- paste(TTravelURL, TKeyPerfDoc, TFormat, fromStop, toStop, fromTime, toTime, sep="")
+    TRequest <- paste(TTravelURL, TKeyAPIDoc, TFormat, fromStop, toStop, fromTime, toTime, sep="")
     foo <- fromJSON(TRequest)[[1]]
 }
 
