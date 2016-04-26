65ee160bc3d30e721f8c0d70d59bb822449c8378
Jeff Cunningham <jefrey@JeffsMBP2.local>
2016-04-26 10:42:19 -0400

    Just getting started with the script to pull MBTA data.

---
+++ fetchMbtaData.R
@@ -0,0 +1,16 @@
+# Just a starting point for our CSCI E-107 group project
+# Step 1 -- pull down json travel time data from MBTA
+# https://groups.google.com/forum/#!topic/massdotdevelopers/z9scDvMCXDo
+
+library(jsonlite)
+library(dplyr)
+library(tidyr)
+library(readr)
+
+
+
+
+
+
+# epoch time
+# https://stackoverflow.com/questions/13456241/convert-unix-epoch-to-date-object-in-r
