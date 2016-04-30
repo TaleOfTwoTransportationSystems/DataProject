c28dc938edbf1a9f215ef20fa5741a9c0c3c5a8e
Jeff Cunningham <jefrey_c@airpost.net>
2016-04-30 17:46:58 -0400

    Getting started with Leaflet (you'll need to install it if you don't already have it).

--- mbta_datamodel.R
+++ mbta_datamodel.R
@@ -2,6 +2,7 @@ library(dplyr)
 library(jsonlite)
 library(lubridate)
 library(readr)
+library(leaflet)
 
 # https://developers.google.com/transit/gtfs/reference#feed-files
 # https://transitfeeds.com/p/mbta/91
@@ -249,9 +250,31 @@ if(length(ls(pattern="schedule_dataset")) == 0) {
 }
 
 
+# Map-based plotting
+#https://rstudio.github.io/leaflet/
 #http://www.r-bloggers.com/plotting-gtfs-data-with-r-2/
 #https://stackoverflow.com/questions/28753444/how-to-create-an-interactive-plot-of-gtfs-data-in-r-using-leaflet
 #https://darrkj.github.io/blog/2015/jul202015/
+
+# Network-based plotting
+#http://rpubs.com/kateto/netviz
+#https://briatte.github.io/ggnet/
+
+RedLineSB <- RedLineRoute$stop[1][[1]]
+RedLineNB <- RedLineRoute$stop[2][[1]]
+
+RedLineSB <- mutate(RedLineSB, stop_lat=as.numeric(stop_lat), stop_lon=as.numeric(stop_lon))
+RedLineSB <- mutate(RedLineNB, stop_lat=as.numeric(stop_lat), stop_lon=as.numeric(stop_lon))
+
+Tmap <- leaflet() %>%
+    addTiles() %>%
+    addMarkers(lat=RedLineSB$stop_lat,
+               lng=RedLineSB$stop_lon,
+               popup=RedLineSB$parent_station_name)
+Tmap
+
+
+
 
 
 
